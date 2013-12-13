3f4c7e316fbadf7c5b61c3057a09a3854262b53a
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-12-13 08:21:24 +0000

    Prepare for Next Iteration. Added SCM to project.clj.

--- nrepl-clojure/project.clj
+++ nrepl-clojure/project.clj
@@ -1,8 +1,10 @@
-(defproject uk.org.russet/nrepl-clojure "1.0.0"
+(defproject uk.org.russet/nrepl-clojure "1.1.0-SNAPSHOT"
   :description "Launch a nrepl client inside protege"
   :license {:name "LGPL"
             :url "http://www.gnu.org/licenses/lgpl-3.0.txt"
             :distribution :repo}
+  :scm {:url "https://github.com/phillord/protege-nrepl.git"
+        :name "git"}
   :url "https://github.com/phillord/protege-nrepl"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.nrepl "0.2.3"]
