2fd207ecd0a718d360fb8af7550e1e4e63a9c369
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-08-07 16:10:37 +0100

    Dependency updates.

--- nrepl-clojure/project.clj
+++ nrepl-clojure/project.clj
@@ -8,6 +8,13 @@
   :url "https://github.com/phillord/protege-nrepl"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.nrepl "0.2.3"]
-                 [edu.stanford.protege/org.protege.editor.core.application 
+                 [edu.stanford.protege/org.protege.editor.core.application
                   "5.0.0-beta-16-SNAPSHOT"]
-                 [com.cemerick/pomegranate "0.3.0"]])
+                 [com.cemerick/pomegranate "0.3.0"]]
+  ;; this is a hack workaround to
+  ;; https://github.com/technomancy/leiningen/issues/1569 which otherwise adds
+  ;; nrepl tools as a test dependency in the pom (which means it doesn't get
+  ;; included in protege-nrepl.
+  ;; This bug has been fixed in 2.4.3
+  ;;:profiles {:base {:dependencies ^:replace []}}
+  )
